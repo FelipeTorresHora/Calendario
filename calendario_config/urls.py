745bb4fc620f3d32e00a7cde96ab56d4c81eec41
felipetorreshora <fhora93@gmail.com>
2025-08-31 12:53:16 -0300

    fix: enviroments

--- calendario_config/urls.py
+++ calendario_config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),  # Django allauth URLs
     path('', include('calendario.urls')),  # Inclui as rotas do app "calendario"
 ]
